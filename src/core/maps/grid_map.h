8dff5d01579929a9c262bfcce7f9b50fed62d2ce
art32fil <art32fil@gmail.com>
2016-08-04 10:55:49 +0300

    №7267 a functional of map expand was derived on little functions

--- src/core/maps/grid_map.h
+++ src/core/maps/grid_map.h
@@ -3,14 +3,27 @@
 
 #include <memory>
 #include <cmath>
+#include <iostream>
 
 #include "cell_occupancy_estimator.h"
 #include "grid_cell_factory.h"
 #include "../geometry_utils.h"
 
 class GridMap {
-public:
+private: //flag constants
+  const int RESIZE_DOWN  = 1; // 0b0001
+  const int RESIZE_UP    = 2; // 0b0010
+  const int RESIZE_LEFT  = 4; // 0b0100
+  const int RESIZE_RIGHT = 8; // 0b1000
+  const int RESIZE_VERT  = 3; // 0b0011
+  const int RESIZE_HORZ  = 12;// 0b1100
+  const int RESIZE_NEED  = 15;// 0b1111
+public: // typedefs
   using Cell = std::shared_ptr<GridCell>;
+private: // typedefs
+  using RowContainer = std::vector<Cell>;
+  using GridMapContainer = std::vector<RowContainer>;
+
 public:
   // TODO: cp, mv ctors, dtor
   GridMap(std::shared_ptr<GridCellFactory> cell_factory):
@@ -19,15 +32,15 @@ public:
     // the map size 1x1 is chosen to delegate an expanded evolution to itself
     _cell_factory(cell_factory), _cells(_height) {
 
-    _zero_x = _width /2;
-    _zero_y = _height/2;
+    _map_center_x = std::floor((float)_width /2);
+    _map_center_y = std::floor((float)_height/2);
 
     for (auto &row : _cells) {
       for (int i = 0; i < _width; i++) {
         row.push_back(cell_factory->create_cell());
       }
     }
-    _far_cell = cell_factory->create_cell();
+    _remote_cell = cell_factory->create_cell();
   }
 
   int width() const { return _width; }
@@ -36,45 +49,22 @@ public:
 
   const std::vector<std::vector<Cell>> cells() const { return _cells; }
 
-  /*!
-   * Updates the cell probability to be occupied with a new occupancy value
-   * If the cell with the input coordinates do not exist in the current map then
-   * the map is resized.
-   */
   void update_cell(const DiscretePoint2D& cell_coord,
                    const Occupancy &new_value, double quality = 1.0) {
     // TODO: bounds check
-    int cell_x = cell_coord.x;
-    int cell_y = cell_coord.y;
-    int width_add, height_add;
-    //find if it needs to resize map
-    width_add  = need_add(cell_x, _zero_x, width());
-    height_add = need_add(cell_y, _zero_y, height());
-
-    //expands map with the exponential rule
-    // states are +100%, +300%, +700% etc ( -100% + [2^n*100%] )
-    if (width_add != 0) {
-      int expand_koef = clp2(width_add/width()+1)-1;
-      if (cell_x+_zero_x < 0)
-        resize(expand_koef*width(),4); //flag 0b0100 - left
-      else
-        resize(expand_koef*width(),8); //flag 0b1000 - right
-    }
-    if (height_add != 0) {
-      int expand_koef = clp2(height_add/height()+1)-1;
-      if (cell_y+_zero_y < 0)
-        resize(expand_koef*height(),1); //flag 0b0001 - down
-      else
-        resize(expand_koef*height(),2); //flag 0b0010 - up
-    }
-    _cells[cell_y+_zero_y][cell_x+_zero_x]->set_value(new_value, quality);
+    resize_if_need(cell_coord);
+    int row = cell_coord.y + _map_center_y;
+    int col = cell_coord.x + _map_center_x;
+    _cells[row][col]->set_value(new_value, quality);
   }
 
   double cell_value(const DiscretePoint2D& cell_coord) const {
-    if (has_cell(cell_coord))
-      return _cells[cell_coord.y+_zero_y][cell_coord.x+_zero_x]->value();
-    else
-      return _far_cell->value();
+    if (has_cell(cell_coord)){
+      int row = cell_coord.y+_map_center_y;
+      int col = cell_coord.x+_map_center_x;
+      return _cells[row][col]->value();
+    }
+    return _remote_cell->value();
   }
 
   DiscretePoint2D world_to_cell(double x, double y) const {
@@ -95,114 +85,128 @@ public:
     return bounds;
   }
 
-  int get_zero_x() const {return _zero_x;}
-  int get_zero_y() const {return _zero_y;}
-private: // methods
-  int need_add(const int cell_coord, const int zero_pos, const int map_bound) {
-    if (cell_coord+zero_pos < 0)
-      return -(cell_coord+zero_pos);
-    if (cell_coord+zero_pos >= map_bound)
-      return (cell_coord+zero_pos) - map_bound + 1;
-    return 0;
-  }
-  /* Finds the nearest upper bound value of degree of 2 to the input integer
-   * clp2(0) = 1
-   * clp2(1) = 2
-   * clp2(2) = 4
-   * clp2(3) = 4
-   * clp2(4) = 8
-   * clp2(5) = 8
+  /*!
+   * Gets the column number in container -
+   * the \f$x\f$ coordinate of the map center
    */
-  long clp2(long x) {
-    long p2=1;
-    while (p2<=x){
+  int get_map_center_x() const {return _map_center_x;}
+  /*!
+   * Gets the row number in container -
+   * the \f$y\f$ coordinate of the map center
+   */
+  int get_map_center_y() const {return _map_center_y;}
+
+private: // methods
+  int expand_value(const int map_bound, const int abs_cell_coord) {
+    if (abs_cell_coord < 0)
+      return (-abs_cell_coord)+map_bound;
+    if (map_bound <= abs_cell_coord)
+      return abs_cell_coord + 1;
+    return map_bound;
+  }
+
+  /* Finds the nearest upper bound value of degree of 2 to the input integer
+   * NOTE: 2-3  are bounded by 4,
+   *       4-7  are bounded by 8,
+   *       8-15 are bounded by 16 etc.
+   */
+  long closest_bounded_power_two(long x) {
+    long p2 = 1;
+    while (p2 <= x){
       p2 <<= 1;
     }
     return p2;
   }
 
   bool has_cell(const DiscretePoint2D& cell_coord) const {
-    return 0 <= (cell_coord.x + _zero_x) && (cell_coord.x + _zero_x) < _width &&
-           0 <= (cell_coord.y + _zero_y) && (cell_coord.y + _zero_y) < _height;
+    return 0 <= (cell_coord.x + _map_center_x)          &&
+                (cell_coord.x + _map_center_x) < _width &&
+           0 <= (cell_coord.y + _map_center_y)          &&
+                (cell_coord.y + _map_center_y) < _height;
   }
 
-  using RowContainer = std::vector<Cell>;
-  using GridMapContainer = std::vector<RowContainer>;
-
-  enum class Direction {UP,DOWN,LEFT,RIGHT};
-
-  void add_empty_cells(const Direction& dir,
-                       const int empty_height = 0,
-                       const int empty_width  = 0) {
-
-    GridMapContainer empty_cells(empty_height, RowContainer(empty_width));
+  void add_empty_rows(const int expand_value, GridMapContainer::iterator pos) {
+    GridMapContainer empty_cells(expand_value, RowContainer(width()));
     for (auto& row : empty_cells)
       for (auto& cell : row)
         cell = _cell_factory->create_cell();
 
-    switch (dir) {
-      case Direction::DOWN:
-        _cells.insert(_cells.begin(),empty_cells.begin(), empty_cells.end());
-        _zero_y  += empty_height;
-        _height += empty_height;
-        break;
+    _cells.insert(pos,
+                  empty_cells.begin(),
+                  empty_cells.end());
+    _height += expand_value;
+  }
 
-      case Direction::UP:
-        _cells.insert(_cells.end(),empty_cells.begin(), empty_cells.end());
-        _height += empty_height;
-        break;
+  void add_empty_cols(const int expand_value,
+                     std::function<RowContainer::iterator(RowContainer&)> pos) {
+    GridMapContainer empty_cells(height(), RowContainer(expand_value));
+    for (auto& row : empty_cells)
+      for (auto& cell : row)
+        cell = _cell_factory->create_cell();
 
-      case Direction::LEFT:
-        for (int i = 0; i < height(); i++)
-          _cells[i].insert(_cells[i].begin(),
-                           empty_cells[i].begin(),empty_cells[i].end());
-        _zero_x += empty_width;
-        _width += empty_width;
-        break;
+    for(int i = 0; i < (int) empty_cells.size();i++)
+      _cells[i].insert(pos(_cells[i]),
+                       empty_cells[i].begin(),
+                       empty_cells[i].end());
+    _width += expand_value;
+  }
 
-      case Direction::RIGHT:
-        for (int i = 0; i < height(); i++)
-          _cells[i].insert(_cells[i].end(),
-                           empty_cells[i].begin(),empty_cells[i].end());
-        _width += empty_width;
-        break;
+  void resize(const int extra_value, const int direction_flags) {
 
-      default:
-        break;
+    if(extra_value <= 0 || !(direction_flags & RESIZE_NEED))
+      return;
+
+    if (direction_flags & RESIZE_DOWN) {
+      add_empty_rows(extra_value,_cells.begin());
+      _map_center_y  += extra_value;
+    }
+    if (direction_flags & RESIZE_UP) {
+      add_empty_rows(extra_value,_cells.end());
+    }
+
+    if (direction_flags & RESIZE_LEFT) {
+      add_empty_cols(extra_value,
+                     [](RowContainer& vector){return vector.begin();} );
+      _map_center_x += extra_value;
+    }
+    if (direction_flags & RESIZE_RIGHT) {
+      add_empty_cols(extra_value,
+                     [](RowContainer& vector){return vector.end();} );
     }
   }
 
-  void resize(const int add_value, const int flags_direction) {
-    if(add_value <= 0 || !(flags_direction & 15))
+  void resize_if_need_in_line(const int directions_flag,
+                              const int abs_cell_coord) {
+    if(directions_flag != RESIZE_HORZ && directions_flag != RESIZE_VERT)
       return;
+    int bound, flag;
+    if (directions_flag == RESIZE_HORZ) {bound = width() ; flag = RESIZE_LEFT;}
+    else                                {bound = height(); flag = RESIZE_DOWN;}
+    //find if it needs to resize map
+    int new_bound = expand_value(bound,abs_cell_coord);
+    if (new_bound == bound)
+      return;
+    //expands map with the exponential rule
+    // states are +100%, +300%, +700% etc ( -100% + [2^n*100%] )
+    int expand_coef = closest_bounded_power_two(new_bound/bound) - 1;
+    if (abs_cell_coord < 0) // resize the beginning of map vector
+      resize(expand_coef*bound,flag);
+    else
+      resize(expand_coef*bound,flag<<1);
+  }
 
-    const int RESIZE_DOWN     = 1; // 0b0001
-    const int RESIZE_UP       = 2; // 0b0010
-    const int RESIZE_LEFT     = 4; // 0b0100
-    const int RESIZE_RIGHT    = 8; // 0b1000
-
-    if (flags_direction & RESIZE_DOWN) {
-      add_empty_cells(Direction::DOWN, add_value, width());
-    }
-    if (flags_direction & RESIZE_UP) {
-      add_empty_cells(Direction::UP,   add_value, width());
-    }
-
-    if (flags_direction & RESIZE_LEFT) {
-      add_empty_cells(Direction::LEFT,  height(),add_value);
-    }
-    if (flags_direction & RESIZE_RIGHT) {
-      add_empty_cells(Direction::RIGHT, height(),add_value);
-    }
+  void resize_if_need(const DiscretePoint2D& cell_coord) {
+    resize_if_need_in_line(RESIZE_HORZ,cell_coord.x+_map_center_x);
+    resize_if_need_in_line(RESIZE_VERT,cell_coord.y+_map_center_y);
   }
 
 private: // fields
   int _width, _height;
-  int _zero_x, _zero_y;
+  int _map_center_x, _map_center_y;
   double _m_per_cell;
-  Cell _far_cell;
+  Cell _remote_cell;
   std::shared_ptr<GridCellFactory> _cell_factory;
-  std::vector<std::vector<Cell>> _cells;
+  GridMapContainer _cells;
 };
 
 #endif
